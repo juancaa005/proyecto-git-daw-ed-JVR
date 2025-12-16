4dd7e97a44e8851f0d95fd10960b2ba2c9000100
Juanca <jvallejor@educantabria.es>
2025-12-16 17:53:26 +0100

    Añadido método saludar

--- mini-proyecto-git/src/Main.java
+++ mini-proyecto-git/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    public static void saludar() {
+    System.out.println("Hola desde feature-java-utils");
+}
+
     public static void main(String[] args) {
         System.out.println("Proyecto iniciado");
     }
